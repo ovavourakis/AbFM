94059013db6faf5be01faa338a200c878b889dec
ovavourakis <ovavourakis@icloud.com>
2024-04-08 15:24:23 +0100

    First version of script to pre-filter the AB dataset.

---
+++ data/preproc/filter_ab_data.py
@@ -0,0 +1,92 @@
+"""
+Script to pre-filter antibody data. Run on many CPU cores.
+
+* use ImmuneBuilder dataset from https://zenodo.org/records/7258553
+* merge with pre-defined train/val/test splits from https://zenodo.org/records/8164693
+* filter out instances with inappropriate loci, intrusive stop codons, frameshifts
+* reconstruct full sequences from ANARCI numbering
+* keep instances where reconstructed sequence matches full sequence in entry
+* keep instances with sequence length between MIN_SEQ_LEN and MAX_SEQ_LEN
+* eliminate sequences with missing PDBs
+
+"""
+import pandas as pd
+import os
+
+from multiprocessing import Pool
+from utils import ANARCIs_to_sequence
+
+MIN_SEQ_LEN = 215
+MAX_SEQ_LEN = 260
+col_drop_filters = ['_aa_', '_alignment_', 'junction', 'cdr', 'fwr', 'fwk', 'fwl']
+
+all_metadata_csv = "/vols/opig/users/vavourakis/data/OAS_models/OAS_paired_all.csv"
+splits_csv = "/vols/opig/users/vavourakis/data/oas_splits.csv"
+strucs_dir = "/vols/opig/users/vavourakis/data/OAS_models/structures"
+out_filtered_csv = "/vols/opig/users/vavourakis/data/OAS_models/OAS_paired_filtered.csv"
+
+def process_row(x):
+    return ANARCIs_to_sequence(x.ANARCI_numbering_heavy, x.ANARCI_numbering_light)
+
+
+if __name__ == '__main__':
+
+    print('\nreading metadata')
+    df = pd.read_csv(all_metadata_csv)
+    df.rename(columns={'full_seq': 'full_seq_orig'}, inplace=True)
+    for filter in col_drop_filters:
+        df.drop([col for col in df.columns if filter in col], axis=1, inplace=True)
+    print('reading splits')
+    splits = pd.read_csv(splits_csv)
+    print('merging')
+    df = pd.merge(df, splits, left_on='ID', right_on='oas_id', suffixes=('', '_drop'))
+    df['oas_id'] = df['ID'].fillna(df['oas_id'])
+    df.drop([col for col in df.columns if 'drop' in col], axis=1, inplace=True)
+
+    print(f'\n{len(df)} entries before filtering')
+
+    print('filtering instances with inappropriate loci, intrusive stop codons, frameshifts')
+    df = df[(df['locus_heavy'] == 'H') & (df['locus_light'].isin(['K', 'L'])) &
+            (df['stop_codon_heavy']=='F') & (df['stop_codon_light']=='F') &
+            (df['vj_in_frame_heavy']=='T') & (df['vj_in_frame_light']=='T')]
+
+    print(f'{len(df)} entries after filtering\n')
+
+    print(f'wrangling sequences')
+    with Pool(processes=24) as pool:
+        results = pool.map(process_row, [row for _, row in df.iterrows()])
+    df2 = pd.DataFrame(results, columns=['fwr1_h', 'cdr1_h', 'fwr2_h', 'cdr2_h', 'fwr3_h', 'cdr3_h', 'fwr4_h',
+                                         'fwr1_l', 'cdr1_l', 'fwr2_l', 'cdr2_l', 'fwr3_l', 'cdr3_l', 'fwr4_l',
+                                         'full_seq', 'cdr_concat', 'fw_concat', 'region_indices', 'seqlen']
+                       )
+    print(f'merging')
+    df2.index = df.index
+    df = pd.concat([df, df2], axis=1)
+    del df2
+
+    print(f'keeing instances where reconstructed sequence matches original full sequence')
+    df = df[df['full_seq'] == df['full_seq_orig']]
+    print(f'{len(df)} entries remaining\n')
+
+    print(f'keeping sequences with length between {MIN_SEQ_LEN} and {MAX_SEQ_LEN}.')
+    df = df[(df['seqlen'] >= MIN_SEQ_LEN) & (df['seqlen'] <= MAX_SEQ_LEN)]
+
+    print(f'\n{len(df)} entries remaining\n')
+
+    print(f'generating and checking pdb paths')
+    df['pdb_path'] = df['oas_id'].apply(lambda x: os.path.join(strucs_dir, f'{x}.pdb'))
+    file_mask = df['pdb_path'].apply(lambda path: os.path.exists(path))
+    df = df[file_mask]
+
+    print(f'\n{len(df)} entries remaining\n')
+
+    cols_to_keep = ['oas_id', 'pdb_path', 'seqlen', 'cluster_ids', 'split', 'full_seq', 'locus_heavy', 'locus_light',
+                    'fwr1_h', 'cdr1_h', 'fwr2_h', 'cdr2_h', 'fwr3_h', 'cdr3_h', 'fwr4_h',
+                    'fwr1_l', 'cdr1_l', 'fwr2_l', 'cdr2_l', 'fwr3_l', 'cdr3_l', 'fwr4_l',
+                    'cdr_concat', 'fw_concat', 'region_indices', 
+                    'ANARCI_numbering_heavy', 'ANARCI_numbering_light']
+    df = df[cols_to_keep]
+
+    print(f'writing to {out_filtered_csv}')
+    df.to_csv(out_filtered_csv, index=False)
+    print('done')
---
+++ data/preproc/utils.py
@@ -0,0 +1,55 @@
+import yaml
+import pandas as pd
+import numpy as np
+
+def ANARCI_to_sequence(anarci_str: str):
+    '''Convert ANARCI numbering string of a single chain to 
+    list of sequences by region along with end indices of each region.'''
+
+    anarci_dict = yaml.load(anarci_str, Loader=yaml.SafeLoader)
+    if 'fwh1' in anarci_dict.keys():
+        keys = ['fwh1', 'cdrh1', 'fwh2', 'cdrh2', 'fwh3', 'cdrh3', 'fwh4']
+    elif 'fwk1' in anarci_dict.keys():
+        keys = ['fwk1', 'cdrk1', 'fwk2', 'cdrk2', 'fwk3', 'cdrk3', 'fwk4']
+    elif 'fwl1' in anarci_dict.keys():
+        keys = ['fwl1', 'cdrl1', 'fwl2', 'cdrl2', 'fwl3', 'cdrl3', 'fwl4']
+    else:
+        raise ValueError('Cannot parse ANARCI string.')
+        
+    seqs = []
+    for key in keys:
+        seqs.append(''.join([i[1] for i in anarci_dict[key].items()]))
+    idx = list(np.cumsum([len(seq) for seq in seqs]))
+
+    return seqs, idx
+
+def ANARCIs_to_sequence(anarci_str_h, anarci_str_l):
+    '''
+    Convert ANARCI numbering strings of heavy and light chains to 
+    list of sequences by region along with end-indices by region and
+    total sequence length. Light chains start at index 1000.
+    
+    Returns:
+        (fwr1_h, cdr1_h, fwr2_h, cdr2_h, fwr3_h, cdr3_h, fwr4_h,
+         fwr1_l, cdr1_l, fwr2_l, cdr2_l, fwr3_l, cdr3_l, fwr4_l,
+         full_seq, cdr_concat, fwk_concat, 
+         (0, end_fwr1_h, end_cdr1_h, end_fwr2_h, end_cdr2_h, end_fwr3_h, end_cdr3_h, end_fwr4_h,
+          1000, end_fwr1_l, end_cdr1_l, end_fwr2_l, end_cdr2_l, end_fwr3_l, end_cdr3_l, end_fwr4_l),
+         seq_len
+        )
+    '''
+         
+    seqs_h, idx_h = ANARCI_to_sequence(anarci_str_h)
+    seqs_l, idx_l = ANARCI_to_sequence(anarci_str_l)
+    idx_l = [i+1000 for i in idx_l]
+
+    cdrs = [seqs_h[1] + seqs_h[3] + seqs_h[5] + seqs_l[1] + seqs_l[3] + seqs_l[5]]
+    fwks = [seqs_h[0] + seqs_h[2] + seqs_h[4] + seqs_l[0] + seqs_l[2] + seqs_l[4]]
+    cdr_concat, fwk_concat, full_seq = [''.join(cdrs)], [''.join(fwks)], [''.join(seqs_h + ['/'] + seqs_l)]
+
+    seqs = seqs_h + seqs_l + full_seq + cdr_concat + fwk_concat
+    seq_len = len(full_seq[0])-1 # don't count the slash that separates heavy from light chain
+    idx = tuple([0] + idx_h + [1000] + idx_l)
+    seqs.extend([idx, seq_len])
+    
+    return tuple(seqs)
