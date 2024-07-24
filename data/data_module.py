36eadc968e3affc84bf68d25956c7c0e3dbe0701
ovavourakis <ovavourakis@icloud.com>
2024-07-24 20:05:45 +0100

    Re-introduced relative index jump and chain_id feature after merge.

--- data/data_module.py
+++ data/data_module.py
@@ -56,11 +56,20 @@ class PdbDataset(Dataset):
         
         res_idx = torch.tensor(processed_feats['residue_index'])
 
-        # # re-index residues starting at 1 (heavy chain) and 1001 (light chain), preserving gaps
+        # re-index residues starting at 1 (VH) and jumping by 50 at start of VL, preserving gaps
+        # (VL indices currently start at by 1001 from process_ab_pdb_files)
+        light_chain_start = torch.argmax((res_idx >= 1000).int()).item()
+        heavy_chain_res_idx = res_idx[:light_chain_start] - torch.min(res_idx[:light_chain_start]) + 1
+        heavy_chain_end = heavy_chain_res_idx[-1].item()
+        light_chain_offset = 50
+        light_chain_res_idx = res_idx[light_chain_start:] - torch.min(res_idx[light_chain_start:]) + (heavy_chain_end + light_chain_offset)
+        res_idx = torch.cat([heavy_chain_res_idx, light_chain_res_idx], dim=0)
+        chain_id = torch.cat([torch.zeros(heavy_chain_res_idx.size(0)), torch.ones(light_chain_res_idx.size(0))], dim=0)
+
+        # alternative to implement danid with absolute jump
         # light_chain_start = torch.argmax((res_idx >= 1000).int()).item()
-        # heavy_chain_res_idx = res_idx[:light_chain_start] - torch.min(res_idx[:light_chain_start]) + 1
-        # light_chain_res_idx = res_idx[light_chain_start:] - torch.min(res_idx[light_chain_start:]) + 1001
-        # res_idx = torch.cat([heavy_chain_res_idx, light_chain_res_idx], dim=0)
+        # chain_id = torch.zeros(len(res_idx))
+        # chain_id[light_chain_start:] = 1
 
         return {
             'file': processed_file_path,
