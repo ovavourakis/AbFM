1ce9111fcf29198c21c8984bdae88091a9928483
ovavourakis <ovavourakis@icloud.com>
2024-08-21 00:46:32 +0100

    Enabled diversity analysis with SPACE2.

---
+++ analysis/refolds/imgt_renumber_pdbs.py
@@ -0,0 +1,46 @@
+""" SPACE2 expects a directory of IMGT-numbered antibody pdbs. 
+    Here, we take our sequence-less generated structures and re-index the residues in the pdbs
+    based on the IMGT numbering of the inverse-folded sequence with the lowest scRMSD. """
+
+import os
+import pandas as pd
+from tqdm import tqdm
+from Bio.PDB import PDBParser, PDBIO, Model, Chain, Residue, Structure
+
+def renumber_pdb(scaffold_pdb_path, imgt_pdb_path, outdir):
+    parser = PDBParser(QUIET=True)
+    # get structures
+    scaffold_structure = parser.get_structure('scaffold', scaffold_pdb_path)
+    imgt_structure = parser.get_structure('imgt', imgt_pdb_path)
+    
+    new_structure = Structure.Structure('new_structure')  # create a new empty structure
+    for scaffold_model, imgt_model in zip(scaffold_structure, imgt_structure):
+        new_model = Model.Model(scaffold_model.id)
+        for scaffold_chain, imgt_chain in zip(scaffold_model, imgt_model):
+            new_chain = Chain.Chain(scaffold_chain.id)
+            for scaffold_residue, imgt_residue in zip(scaffold_chain, imgt_chain):
+                new_residue = Residue.Residue(imgt_residue.id, scaffold_residue.resname, scaffold_residue.segid)
+                for atom in scaffold_residue:
+                    new_residue.add(atom.copy())
+                new_chain.add(new_residue)
+            new_model.add(new_chain)
+        new_structure.add(new_model)
+    
+    io = PDBIO()
+    io.set_structure(new_structure)
+    struc = '_'.join(os.path.basename(imgt_pdb_path).split('_')[:-1] + ['gen.pdb'])
+    output_path = os.path.join(outdir, struc)
+    io.save(output_path, preserve_atom_numbering=True)
+
+
+gen_dir = '/vols/opig/users/vavourakis/generations/newclust_newsample_newindex_fullrun'
+
+out_dir = os.path.join(gen_dir, 'renumbered_pdbs')
+if not os.path.exists(out_dir):
+    os.makedirs(out_dir)
+best_rmsds_path = os.path.join(gen_dir, 'designed_seqs', 'best_rmsds.csv')
+best_rmsds = pd.read_csv(best_rmsds_path)
+
+print('IMGT-numbering generated PDBs based on best re-folded structures...')
+tqdm.pandas(desc="Renumbering PDBs")
+best_rmsds.progress_apply(lambda row: renumber_pdb(row['scaffold_path'], row['refolded_path'], out_dir), axis=1)
---
+++ analysis/refolds/space.py
@@ -0,0 +1,43 @@
+"""
+Calculates pairwise RMSDs among length-homogenous CDRs for each CDR type using IMGT-numbered PDB files.
+Generate a violin plot to visualize the distribution of RMSD values for each CDR type.
+
+Usage:
+    python space.py
+"""
+
+import glob, os
+import numpy as np
+import pandas as pd
+from tqdm import tqdm
+from SPACE2.util import reg_def
+from SPACE2.exhaustive_clustering import get_distance_matrices
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# define IMGT-numbered pdb files
+gen_dir = '/vols/opig/users/vavourakis/generations/newclust_newsample_newindex_fullrun'
+pdb_files = glob.glob(os.path.join(gen_dir, 'renumbered_pdbs', '*.pdb'))
+
+# calculate pairwise RMSDs among length-homogenous CDRs for each CDR type
+cdrs = ['CDRH1', 'CDRH2', 'CDRH3', 'CDRL1', 'CDRL2', 'CDRL3']
+rmsd_matrices = {cdr: {} for cdr in cdrs}
+for cdr in tqdm(['CDRH1', 'CDRH2', 'CDRH3', 'CDRL1', 'CDRL2', 'CDRL3']):
+    selection = [reg_def[cdr]]
+    out = get_distance_matrices(pdb_files, selection=selection, anchors=selection, n_jobs=-1)
+    rmsd_matrices[cdr] = [val[1] for key, val in out.items()] 
+
+# convert to flat list of rmsd values per cdr
+rmsd_values = {cdr: [value for matrix in rmsd_matrices[cdr] for value in matrix[np.triu_indices(len(matrix), k=1)]] for cdr in cdrs}
+
+# plot
+plt.figure(figsize=(10, 6))
+sns.violinplot(data=[rmsd_values[cdr] for cdr in cdrs], palette="Set3", cut=0)
+plt.xticks(ticks=range(len(cdrs)), labels=cdrs)
+plt.ylabel('RMSD (Angstrom)')
+plt.title('Pairwise inter-sample RMSDs for same-length CDRs', fontsize=16)
+outfile = os.path.join(gen_dir, 'designed_seqs', 'pw_rmsd_violinplot.png')
+plt.tight_layout()
+plt.savefig(outfile)
+
